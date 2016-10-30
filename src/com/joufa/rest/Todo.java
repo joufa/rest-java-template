06a73c1718caa239ad50cbf47c7e9cff23358e99
Antti Reijonen <areijone@gmail.com>
2016-10-30 12:50:20 +0200

    Added Todo model

---
+++ src/com/joufa/rest/Todo.java
@@ -0,0 +1,21 @@
+package com.joufa.rest;
+
+
+public class Todo {
+	private String name;
+	private String description;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return description;
+	}
+	public void setDesc(String description) {
+		this.description = description;
+	}
+
+
+}
